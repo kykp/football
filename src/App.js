c934ec24503edf2443c8fdf062873d423db926bc
kykp <kykp@yandex.ru>
2022-05-10 14:21:33 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  var myHeaders = new Headers();
+  myHeaders.append("x-rapidapi-key", "2b0e52bae0c0f1d318ed22c205fe264a");
+  myHeaders.append("x-rapidapi-host", "v3.football.api-sports.io");
+
+  var requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  fetch("https://v3.football.api-sports.io/status", requestOptions)
+    .then((response) => response.json())
+    .then((result) => console.log(result))
+    .catch((error) => console.log("error", error));
+
+  return <div className="App">Hello React</div>;
 }
 
 export default App;
